9952ba809948f4b55f23da7649a9ffa035abc3be
childcity <n.gorodetskiy@devcaz.com>
2020-08-10 19:18:56 +0300

    add Board class

---
+++ src/dal/board.h
@@ -0,0 +1,156 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "tiledata.h"
+
+#include <QVector>
+#include <algorithm>
+#include <random>
+
+namespace DAL {
+
+
+class Board {
+
+    using Position = std::pair<int, int>;
+
+    enum class ShiftDirection { Right, Left, Top, Bottom };
+
+public:
+    Board(int dimension = defaultDimension_)
+        : dimension_(dimension)
+    {
+        boardElements_.resize(dimension_ * dimension_);
+
+        // fill with 1,2,3,4,5,...,0
+        std::iota(boardElements_.begin(), boardElements_.end() - 1, 1);
+        boardElements_.last() = hiddenValue();
+
+        shaffleBoard();
+    }
+
+    int hiddenValue() const { return 0; }
+
+    bool isGameWon() const
+    {
+      return boardElements_.first().Value != hiddenValue()
+             && std::is_sorted(boardElements_.begin(), boardElements_.end() - 1);
+    }
+
+private:
+    Position getRowCol(int index) const { return {index / dimension_, index % dimension_}; }
+
+    int getIndex(Position pos) const { return pos.first * dimension_ + pos.second; }
+
+    void shaffleBoard()
+    {
+        using std::chrono::system_clock;
+        static auto seed = system_clock::now().time_since_epoch().count();
+        static std::mt19937 generator(seed);
+
+        do {
+            std::shuffle(boardElements_.begin(), boardElements_.end(), generator);
+            //DEBUG("isBoardSolvable" <<isBoardSolvable())
+        } while ((! isBoardSolvable()) || isGameWon());
+    }
+
+    bool isMovable(Position pos, Position hidPos) const
+    {
+        return  pos.second == hidPos.second || pos.first == hidPos.first;
+    }
+
+    bool isBoardSolvable() const
+    {
+      // Count inversions in given puzzle
+      int invCount = 0;
+      for (int i = 0; i < boardElements_.size() - 1; i++)
+      {
+          for (int j = i + 1; j < boardElements_.size(); j++)
+          {
+              // count pairs(i, j) such that i appears
+              // before j, but i > j.
+              if (boardElements_[j].Value && boardElements_[i].Value
+                  && boardElements_[i] > boardElements_[j])
+              {
+                  invCount++;
+              }
+          }
+      }
+
+      auto isOdd = [](int val){ return (val & 1); };
+      auto isEven = [&isOdd](int val){ return ! isOdd(val); };
+
+      // If grid is odd, return true if inversion
+      // count is even.
+      if (isOdd(dimension_)) {
+          return isEven(invCount);
+      } else {
+          // grid is even
+          const int hiddenIndex = findHiddenIndex();
+          const int pos = getRowCol(hiddenIndex).first;
+          return isOdd(pos) ? isEven(invCount) : isOdd(invCount);
+      }
+    }
+
+    int findHiddenIndex() const
+    {
+      // find and update hidden index
+      const auto it = std::find(boardElements_.begin(), boardElements_.end(), TileData(hiddenValue()));
+      assert(it != boardElements_.end());
+      return static_cast<int>(std::distance(boardElements_.begin(), it));
+    }
+
+    void shift2D(QVector<TileData> &vec, Position strPos, int count, ShiftDirection direction)
+    {
+        int row, col;
+        std::tie(row, col) = strPos;
+
+        assert(row < dimension_);
+        assert(col < dimension_);
+
+        if (direction == ShiftDirection::Right) {
+            int strtIndx = col + 1;
+            int endIndx = col + count;
+            assert(endIndx < dimension_);
+
+            for (int i = strtIndx; i <= endIndx; ++i) {
+                vec[getIndex({row, i - 1})].swap(vec[getIndex({row, i})]);
+            }
+        } else if (direction == ShiftDirection::Left) {
+            int strtIndx = col - 1;
+            int endIndx = col - count;
+            assert(endIndx >= 0);
+
+            for (int i = strtIndx; i >= endIndx; --i) {
+                vec[getIndex({row, i + 1})].swap(vec[getIndex({row, i})]);
+            }
+        } else if (direction == ShiftDirection::Bottom) {
+            int strtIndx = row + 1;
+            int endIndx = row + count;
+            assert(endIndx < dimension_);
+
+            for (int i = strtIndx; i <= endIndx; ++i) {
+                vec[getIndex({i - 1, col})].swap(vec[getIndex({i, col})]);
+            }
+        } else if (direction == ShiftDirection::Top) {
+            int strtIndx = row - 1;
+            int endIndx = row - count;
+            assert(endIndx >= 0);
+
+            for (int i = strtIndx; i >= endIndx; --i) {
+                vec[getIndex({i + 1, col})].swap(vec[getIndex({i, col})]);
+            }
+        }
+    }
+
+private:
+    static constexpr int defaultDimension_ = 4;
+
+    int dimension_;
+    QVector<TileData> boardElements_;
+};
+
+
+}
+
+#endif // BOARD_H
